2ba8737fc6bfafc673524620f6704444f704761a
ABogunets <abogunets@gmail.com>
2023-03-09 23:39:32 +0200

    h2 render condition added in statsTitle

--- src/components/Statistics/StatsTitle/StatsTitle.jsx
+++ src/components/Statistics/StatsTitle/StatsTitle.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import {Title} from 'components/Statistics/StatsTitle/StatsTitle.styled';
 
-export const StatsTitle = ({title}) => {
-  return <Title>{title}</Title>
+export const StatsTitle = ({ title }) => {
+  return (
+    title && (<Title>{title}</Title >)
+  )
 }
 
 StatsTitle.propTypes = {
